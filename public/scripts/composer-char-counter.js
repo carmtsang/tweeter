a1b146af77c197889633cb88dd3adf326aba78ec
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-15 23:51:23 +0000

    counter turn red in -ve nums

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,24 @@
 $(document).ready(() => {
-  console.log('boom')
-
   const $input = $('#tweet-text');
 
   $input.on('input', function() {
+    // currentInput = what is typed into the textbox
     const $currentInput = $(this).val()
-    console.log($currentInput.length)
     let $count = Number($('.counter strong').text());
     
+    // +1 input = -1 count & -1 input = +1 count
     if ($currentInput.length + 1) {
       $count -= 1;
     } else if ($currentInput.length - 1) {
       $count +=1
-    }
-    console.log($count)
-    $('.counter strong').text($count)
-  })
+    };
+    // counter red when less than 0
+    if ($count < 0) {
+      $('.counter strong').css('color', 'red');
+    };
+
+    $('.counter strong').text($count);
+  });
 
 });
 
