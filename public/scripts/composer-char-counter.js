a35da8ccb0a90411941ffc1b93c7375101b8c0b8
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-18 03:09:51 +0000

    clean up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,19 @@
 $(document).ready(() => {
-  console.log('boom')
+  console.log('boom');
 
   const $input = $('#tweet-text');
   
   $input.on('input', function() {
-    const inputLength = $(this).val().length
+    const inputLength = $(this).val().length;
     const $counter = $(this).next().children('.counter');
    
     const maxLength = 140;
-    
     const remaining = maxLength - inputLength;
 
     // counter turns red when negative
     (remaining < 0) ? $counter.css('color', 'red') : $counter.css('color', 'black');
   
-    
-    $counter.text(remaining)
+    $counter.text(remaining);
   })
 
 });
