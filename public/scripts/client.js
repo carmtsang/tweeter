a54d2626a658e536ddef99099680f15052e02a7e
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-19 02:01:26 +0000

    add test data & start template for createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+}
+
+const createTweetElement = function(tweet) {
+let $tweet = 
+`<article class="tweet-container">
+<header>
+  <div class="user">
+    <img src="https://i.imgur.com/73hZDYK.png">
+    Newton
+  </div>
+  <p class="handle">@SirIssac</p>
+</header>
+<div class="tweet-body">
+  <p>What goes up must come down.</p>
+</div>
+<footer>
+  <span>x days ago</span>
+  <div class="tweet-icons">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+  </div>
+</footer>
+</article> `
+return $tweet;
+}
+
+renderTweets(data);
