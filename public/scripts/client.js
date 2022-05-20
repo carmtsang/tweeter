45218f51e093a4a62757e09590fdd3eaf4ebb8b6
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-20 02:37:28 +0000

    create loadTweet function & set loadTweet after form submittion

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,3 @@
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(() => {
   const renderTweets = tweets => {
     for (const tweet of tweets) {
@@ -55,14 +28,20 @@ $(() => {
       </article>`;
     return $tweet;
   };
-  
-  renderTweets(data);
 
+  const loadTweet = () => {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(tweets => renderTweets(tweets));
+  };
+
+  loadTweet()
+  
+  
+  // Submitting tweets
   $('form').submit(function(event) {
     event.preventDefault();
-    console.log($(this).serialize())
     const $data = $(this).serialize();
-    $.post('/tweets', $data);
-  })
 
+    $.post('/tweets', $data)
+  })
 });
