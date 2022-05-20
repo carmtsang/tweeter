2977a6b2b8a1b1b67d5a22abf49da8f251bb9d97
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-20 21:17:01 +0000

    create escape function to prevent xss, applied to tweet content

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,14 @@ $(() => {
     }
   };
   
+  const escape = str => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   const createTweetElement = tweet => {
+    const content = escape(tweet.content.text);
     let $tweet =
       `<article>
         <header>
@@ -16,7 +23,7 @@ $(() => {
           </div>
           <p class="handle">${tweet.user.handle}</p>
         </header>
-        <p class="tweet-body">${tweet.content.text}</p>
+        <p class="tweet-body">${content}</p>
         <footer>
           <span>${timeago.format(tweet.created_at)}</span>
           <div class="tweet-icons">
@@ -31,26 +38,27 @@ $(() => {
 
   const loadTweet = () => {
     $.ajax('/tweets', { method: 'GET' })
-    .then(tweets => renderTweets(tweets));
+      .then(tweets => renderTweets(tweets));
   };
 
   // inital load of the tweets on the page
-  loadTweet()
+  loadTweet();
   
   // Submitting tweets
   $('form').submit(function(event) {
     event.preventDefault();
     const $data = $(this).serialize();
+  
     const $length = $(this).children('#tweet-text').val().length;
+
     if ($length > 140) {
       alert('Tweet is too long');
     } else if (!$length) {
-      alert('Please input a tweet')
+      alert('Please input a tweet');
     } else {
       $.post('/tweets', $data)
-      .then(() =>$(this).trigger('reset') && $('.counter').text(140))
-      .then(() => loadTweet());
+        .then(() =>$(this).trigger('reset') && $('.counter').text(140))
+        .then(() => loadTweet());
     }
-
-  })
+  });
 });
