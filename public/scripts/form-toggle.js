de1564b93cb6dd569d7813a15ac9c0ba77402f4e
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-21 22:42:14 +0000

    scroll to top & hide button

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -2,19 +2,22 @@ $(() => {
   $('.new-tweet').hide();
   $('#scroll-up').hide();
   
+  // toggle new tweet section using right nav button
   $('.nav-right').click(function() {
     $('.new-tweet').toggle()
   })
 
+ // if window is at top, hide button, else show button
   $(window).scroll(function() {
-    $('#scroll-up').show();
+    if ($(window).scrollTop() === 0) {
+      $('#scroll-up').hide()
+    } else {
+      $('#scroll-up').show();
+    }
   })
 
+  // scroll to top when clicked
   $('#scroll-up').click(function() {
     $(window).scrollTop(0, 0);
   })
-
-
-
-
 });
